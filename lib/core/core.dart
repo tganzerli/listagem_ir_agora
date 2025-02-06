138ff9e636619589ca88853c24ded8f749aeae4e
Thiago Ganzerli <96201486+tganzerli@users.noreply.github.com>
2025-02-06 13:34:45 -0300

    Include injector in core
    adding the injector to the core to enable dependency invention

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -1,4 +1,18 @@
+import 'package:auto_injector/auto_injector.dart';
+import 'package:http/http.dart' as http;
+
+import 'core.dart';
+import 'services/client/http/http_impl.dart';
+
 export 'contracts/contracts.dart';
 export 'errors/errors.dart';
 export 'output/output.dart';
 export 'services/services.dart';
+
+final core = AutoInjector(
+  tag: 'core',
+  on: (i) {
+    i.add<http.Client>(http.Client.new);
+    i.add<RestClient>(RestClientHttp.new);
+  },
+);
--- lib/injector.dart
+++ lib/injector.dart
@@ -1,5 +1,8 @@
 import 'package:auto_injector/auto_injector.dart';
 
+import 'core/core.dart';
+
 final injector = AutoInjector(on: (i) {
+  i.addInjector(core);
   i.commit();
 });
