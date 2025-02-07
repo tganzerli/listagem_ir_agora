2543b0d875564b4cd7ec3ee4e80e5fa95a22fcdc
Thiago Ganzerli <96201486+tganzerli@users.noreply.github.com>
2025-02-07 20:15:47 -0300

    Start the UI
    update  and clean the main(),
    update the injector for a function
    add AppWidget and GoNowPage empty class

--- lib/injector.dart
+++ lib/injector.dart
@@ -2,7 +2,9 @@ import 'package:auto_injector/auto_injector.dart';
 
 import 'core/core.dart';
 
-final injector = AutoInjector(on: (i) {
-  i.addInjector(core);
-  i.commit();
-});
+final injector = AutoInjector();
+
+void setupDependencies() {
+  injector.addInjector(core);
+  injector.commit();
+}
---
+++ lib/ui/go_now/go_now_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GoNowPage extends StatefulWidget {
+  const GoNowPage({super.key});
+
+  @override
+  State<GoNowPage> createState() => _GoNowPageState();
+}
+
+class _GoNowPageState extends State<GoNowPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
