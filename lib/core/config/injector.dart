7ec0734d6c23b9d848de044f71c429510d3ae65a
Thiago Ganzerli <96201486+tganzerli@users.noreply.github.com>
2025-02-11 19:16:20 -0300

    Update injector.dart

--- lib/core/config/injector.dart
+++ lib/core/config/injector.dart
@@ -1,12 +1,14 @@
 import 'package:auto_injector/auto_injector.dart';
 import 'package:http/http.dart' as http;
+import 'package:lista_ir_agora/ui/pages/go_now/widget/featured_motels/viewmodels/featured_motels_viewmodels.dart';
 
+import '../../ui/pages/go_now/widget/motel_card/viewmodels/motel_card_viewmodels.dart';
 import '../core.dart';
 import '../services/client/http/http_impl.dart';
 import '../../data/datasources/motels/motels_client_http.dart';
 import '../../data/repositories/motels/motels_repository.dart';
 import '../../data/repositories/motels/remote_motels_repository.dart';
-import '../../ui/go_now/viewmodels/go_now_viewmodels.dart';
+import '../../ui/pages/go_now/viewmodels/go_now_viewmodels.dart';
 
 final injector = AutoInjector();
 
@@ -16,6 +18,7 @@ void setupDependencies() {
   injector.add<MotelsClientHttp>(MotelsClientHttp.new);
   injector.add<MotelsRepository>(RemoteMotelsRepository.new);
   injector.add<GoNowViewModel>(GoNowViewModel.new);
-
+  injector.add<MotelCardViewmodels>(MotelCardViewmodels.new);
+  injector.add<FeaturedMotelsViewmodels>(FeaturedMotelsViewmodels.new);
   injector.commit();
 }
