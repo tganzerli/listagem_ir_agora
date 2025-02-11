0e6b58bd902e54eb9fb27f62c1282c656b3d0b10
Thiago Ganzerli <96201486+tganzerli@users.noreply.github.com>
2025-02-11 19:16:24 -0300

    Update app_widget.dart

--- lib/ui/app_widget.dart
+++ lib/ui/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lista_ir_agora/layout/layout.dart';
 
-import 'go_now/go_now_page.dart';
+import 'pages/go_now/go_now_page.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
@@ -9,6 +10,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: lightTheme(context),
       routes: {
         '/': (context) => const GoNowPage(),
       },
